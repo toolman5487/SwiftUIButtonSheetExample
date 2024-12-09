ce6a6236a17e1d89499ab07ff2b3726af6eabcef
Willy <willy548798@gmail.com>
2024-12-09 17:25:18 +0800

    Initial Commit

---
+++ ButtonSheet/ButtonSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonSheetApp.swift
+//  ButtonSheet
+//
+//  Created by Willy Hsu on 2024/12/9.
+//
+
+import SwiftUI
+
+@main
+struct ButtonSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ButtonSheet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ButtonSheet
+//
+//  Created by Willy Hsu on 2024/12/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
