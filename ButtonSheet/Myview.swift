432d2e19ea3688864160810ded7d40e4969fdb1a
Willy <willy548798@gmail.com>
2024-12-10 16:04:21 +0800

    buttonsheet v2

---
+++ ButtonSheet/Myview.swift
@@ -0,0 +1,18 @@
+//
+//  Myview.swift
+//  ButtonSheet
+//
+//  Created by Willy Hsu on 2024/12/9.
+//
+
+import SwiftUI
+
+struct Myview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Myview()
+}
