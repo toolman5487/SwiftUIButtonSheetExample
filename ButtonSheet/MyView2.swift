58a14bec6bafb6b95461e71311a804b68adee1a3
Willy <willy548798@gmail.com>
2024-12-17 15:08:49 +0800

    SwiftUI TabView v1

---
+++ ButtonSheet/MyView2.swift
@@ -0,0 +1,18 @@
+//
+//  MyView2.swift
+//  ButtonSheet
+//
+//  Created by Willy Hsu on 2024/12/17.
+//
+
+import SwiftUI
+
+struct MyView2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyView2()
+}
